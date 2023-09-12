01119f53e7a65617dbf88bb206af233c73017c4f
Dhananjaya-Herath <ar98384@fhss.sjp.ac.lk>
2023-09-12 08:07:13 +0530

    registration success

--- lib/RegisterForm.cs
+++ lib/RegisterForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace lib
 {
     public partial class RegisterForm : Form
     {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS\Documents\library.mdf;Integrated Security=True;Connect Timeout=30");
         public RegisterForm()
         {
             InitializeComponent();
@@ -31,7 +34,69 @@ namespace lib
 
         private void Register_btn_Click(object sender, EventArgs e)
         {
+            if (register_email.Text == "" || register_username.Text == "" || register_password.Text == "")
+            {
+                MessageBox.Show("Please fill all blank fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        connect.Open();
 
+                        String checkUsername = "SELECT COUNT(*) FROM users WHERE username = @username";
+
+                        using (SqlCommand checkCMD = new SqlCommand(checkUsername, connect))
+                        {
+                            checkCMD.Parameters.AddWithValue("@username", register_username.Text.Trim());
+                            int count = (int)checkCMD.ExecuteScalar();
+
+                            if (count >= 1)
+                            {
+                                MessageBox.Show(register_username.Text.Trim()
+                                    + " is already taken", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                // TO GET THE DATE TODAY
+                                DateTime day = DateTime.Today;
+
+                                String insertData = "INSERT INTO users (email, username, password, date_register) " +
+                                    "VALUES(@email, @username, @password, @date)";
+
+                                using (SqlCommand insertCMD = new SqlCommand(insertData, connect))
+                                {
+                                    insertCMD.Parameters.AddWithValue("@email", register_email.Text.Trim());
+                                    insertCMD.Parameters.AddWithValue("@username", register_username.Text.Trim());
+                                    insertCMD.Parameters.AddWithValue("@password", register_password.Text.Trim());
+                                    insertCMD.Parameters.AddWithValue("@date", day.ToString());
+
+                                    insertCMD.ExecuteNonQuery();
+
+                                    MessageBox.Show("Register successfully!", "Information Message"
+                                        , MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                    LoginForm lForm = new LoginForm();
+                                    lForm.Show();
+                                    this.Hide();
+                                }
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error connecting Database: " + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+            }
         }
 
         private void register_showPass_CheckedChanged(object sender, EventArgs e)
